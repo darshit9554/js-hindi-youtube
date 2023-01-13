8ef850b55a5b96e24e090458e624dac9ebad8c77
darshit9554 <121384090+darshit9554@users.noreply.github.com>
2023-01-13 09:26:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat whole code as newer version
+
+//alert(3+3)  // we are using nodejs,not browser
+
+console.log(3
+    +
+    3)  //code readability is zero
+
+/*
+     primitive datatypes
+
+     1-number=> 2 tp power 53
+     2-bigint
+     3-string=>""
+     4-boolean=>true/false
+     5-null=>standalone value
+     6-undefined=>
+     7-symbol =>unique
+     */
+
+//object
+
+console.log(typeof 2.453);
+console.log(typeof "Darshit")
+console.log(typeof null) // language ki khami hai=> null ka datatype object
+console.log(typeof undefined) // datatype=undefined
